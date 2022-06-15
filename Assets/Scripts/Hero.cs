23dc278181da38b373771513421ed4fd55daf694
TheManOfShit <kitenyan@gmail.com>
2022-06-15 17:14:06 +0300

    Init

---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : Entity
+{
+
+    [SerializeField] private float speed = 3f;
+    [SerializeField] private int lives = 5;
+    [SerializeField] private float jumpForse = 0.5f;
+    private bool isGrounded = false;
+
+    private Rigidbody2D rb;
+    private Animator anim;
+    private SpriteRenderer sprite;
+
+    public static Hero Instance { get; set; }
+
+    private States State
+    {
+        get { return (States)anim.GetInteger("state");  }
+        set { anim.SetInteger("state", (int)value); }
+    }
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+        Instance = this;
+    }
+
+    private void FixedUpdate()
+    {
+        CheckGround();
+    }
+
+    void Update()
+    {
+        if (isGrounded) State = States.idle;
+
+        if (Input.GetButton("Horizontal"))
+            Run();
+        if (isGrounded && Input.GetButton("Jump"))
+            Jump();
+    }
+
+    private void Run()
+    {
+        if (isGrounded) State = States.run;
+
+        Vector3 dir = transform.right * Input.GetAxis("Horizontal");
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, speed * Time.deltaTime);
+        sprite.flipX = dir.x < 0.0f;
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(transform.up * jumpForse, ForceMode2D.Impulse);
+    }
+
+    private void CheckGround()
+    {
+        Collider2D[] collider = Physics2D.OverlapCircleAll(transform.position, 0.3f);
+        isGrounded = collider.Length > 1;
+
+        if (!isGrounded) State = States.jump;
+    }
+
+    public override void GetDamage()
+    {
+        lives -= 1;
+        Debug.Log(lives); 
+    }
+}
+
+public enum States
+{
+    idle,
+    run,
+    jump
+}
---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject == Hero.Instance.gameObject)
+        {
+            Hero.Instance.GetDamage();
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
